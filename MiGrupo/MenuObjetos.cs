f7455010382dc82e31e3e51d5c7d04fce82814a5
GastonTS <gaston-ts@hotmail.com>
2015-05-11 17:28:48 -0300

    Ordeno un poco la clase menu y escalo las texturas

--- MiGrupo/MenuObjetos.cs
+++ MiGrupo/MenuObjetos.cs
@@ -17,73 +17,60 @@ using TgcViewer.Utils.TgcSceneLoader;
 
 namespace AlumnoEjemplos.MiGrupo
 {
-    class MenuObjetos
+    class MenuObjetos 
     {
         private TgcSprite menu;
         private TgcSprite[] objetos;
         Size screenSize = GuiController.Instance.Panel3d.Size;
-
-        private string texturaMenu()
-        {
-            return EjemploAlumno.alumnoTextureFolder() + "menu3.jpg"; 
-        }
-
-        private string texturaObjeto()
-        {
-            return EjemploAlumno.alumnoTextureFolder() + "menu2.jpg";
-        }
-
+        TgcTexture texturaMenu = TgcTexture.createTexture(EjemploAlumno.alumnoTextureFolder() + "menu3.jpg");
 
         public MenuObjetos()
         {               
-            //Crear Sprite
+            //Creacion de Sprites
             menu = new TgcSprite();
-            menu.Texture = TgcTexture.createTexture(texturaMenu());
-            objetos = new TgcSprite[10];
-            for(var i = 0; i < 10; i++)
+            objetos = new TgcSprite[8];
+            for(var i = 0; i < 8; i++)
                 objetos[i] = new TgcSprite();
+
+            //Asginar Texturas
+            menu.Texture = texturaMenu;
             foreach (var sprite in objetos)
-                sprite.Texture = TgcTexture.createTexture(texturaObjeto());
-
-            //Ubicarlo centrado en la pantalla
-            menu.Position = new Vector2(screenSize.Width * 0.8f, 0);
-            menu.SrcRect = new Rectangle(100, 100, (int)Math.Round(screenSize.Width * 0.2), screenSize.Height);
-
-                       
-            //Modifiers para variar parametros del sprite
-            GuiController.Instance.Modifiers.addFloat("Tamaño ancho en % de Screen", 0, 1, 0.2f);
-        
+                sprite.Texture = TgcTexture.createTexture(EjemploAlumno.alumnoTextureFolder() + "menu2.jpg");      
         }
 
         public void renderMenu()
         {
             menu.render();
-            for (var i = 0; i < 10; i++)
+            for (var i = 0; i < 8; i++)
                 objetos[i].render();
         }
 
         public void disposeMenu()
         {
             menu.dispose();
+            for (var i = 0; i < 8; i++)
+                objetos[i].dispose();
         }
 
         public void actualizarModifiers()
-        {
-            float heightRate = (float) GuiController.Instance.Modifiers["Tamaño ancho en % de Screen"];
-            float esquinaSupIzqMenu = screenSize.Width * (1 - heightRate);
-            int ladoObjeto = (int)Math.Round(screenSize.Width * heightRate * 0.5f);
-            //Actualizar valores cargados en modifiers
-            menu.Position = new Vector2(esquinaSupIzqMenu, 0);
-            menu.SrcRect = new Rectangle(100, 100, (int)Math.Round(screenSize.Width * heightRate), screenSize.Height);
-            for (var i = 0; i < 5; i++)
+        {//Esta pensado para una resolucion con aspect ratio 16:9
+            //Actualizacion de Menu
+            float ladoIzqMenu = screenSize.Width * (1 - 0.25f);
+            float anchoMenu = screenSize.Width - ladoIzqMenu;
+            menu.Position = new Vector2(ladoIzqMenu, 0);
+            menu.SrcRect = new Rectangle(0, 0, (int)Math.Round(anchoMenu), screenSize.Height);
+            
+            //Actualizacion de Objetos de Menu
+            float ladoObjeto = anchoMenu *0.5f;
+            for (var i = 0; i < 4; i++)
             {
-                objetos[i].Position = new Vector2(esquinaSupIzqMenu, 0 + ladoObjeto * i);
-                objetos[i].SrcRect = new Rectangle(100, 100, ladoObjeto, ladoObjeto);
+                objetos[i].Position = new Vector2(ladoIzqMenu, 0 + ladoObjeto * i);
+                objetos[i].Scaling = new Vector2(ladoObjeto / objetos[0].Texture.Width, ladoObjeto / objetos[0].Texture.Height);
             }
-            for (var i = 5; i < 10; i++)
+            for (var i = 4; i < 8; i++)
             {
-                objetos[i].Position = new Vector2(esquinaSupIzqMenu + ladoObjeto, 0 + ladoObjeto * (i-5));
-                objetos[i].SrcRect = new Rectangle(100, 100, ladoObjeto, ladoObjeto);
+                objetos[i].Position = new Vector2(ladoIzqMenu + ladoObjeto, 0 + ladoObjeto * (i - 4));
+                objetos[i].Scaling = new Vector2(ladoObjeto / objetos[0].Texture.Width, ladoObjeto / objetos[0].Texture.Height);
             }
             
         }
