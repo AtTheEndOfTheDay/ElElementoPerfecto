1a5b6b0e64e688b9ada7d6ac0a97f760098f43cb
GastonTS <gaston-ts@hotmail.com>
2015-05-20 14:19:40 -0300

    Action! (Chau Etapa)

--- MiGrupo/Juego/EjemploAlumno.cs
+++ MiGrupo/Juego/EjemploAlumno.cs
@@ -50,12 +50,21 @@ namespace AlumnoEjemplos.MiGrupo
         MenuObjetos menu;
         TgcBox contenedor;
         Pelota pelota;
+        List<Item> items = new List<Item>();
         List<Item> itemsDelUsuario = new List<Item>();
         List<Item> itemsDeNivel = new List<Item>();
-        //Action<TgcD3dInput, float> etapa;  TODO: revisar...no puedo inicializar una vez al principio me falta el elapsed time
-        Etapa etapa;
-        Construccion construccion;
-        Play play;
+
+        Action<TgcD3dInput, float> etapa;
+        Vector2 mouseVector;
+        Vector2 anteriorMouse;
+        Vector3 movimientoObjeto;
+        Item objetoPickeado;
+        Item objetoAnterior;
+        TgcPickingRay pickingRay = new TgcPickingRay();
+        bool selected = false;
+        bool agarrado = false;
+        Vector3 collisionPoint;
+
         TgcText2d textStage = new TgcText2d();
 
         public static bool mostrarOBBs=false;
@@ -113,23 +122,129 @@ namespace AlumnoEjemplos.MiGrupo
             pelota = fabricaDeNiveles.pelotaNivel(numeroDeNivel);
             fabricaDeNiveles.iniciarNivel(numeroDeNivel, itemsDeNivel, itemsDelUsuario, menu, fondo, contenedor);
             menu = fabricaDeNiveles.menuNivel(itemsDelUsuario);
-            construccion = new Construccion(itemsDelUsuario, pelota, menu);
-            play = new Play(itemsDeNivel, itemsDelUsuario, pelota);
 
             etapa = construccion;
             textStage.Color = Color.White;
             textStage.Position = new Point(0, 0);
             textStage.Text = "Construccion";
 
-            foreach (Item objeto in itemsDelUsuario)
+            items = new List<Item>();
+            foreach (Item item in itemsDeNivel)
+                items.Add(item);
+            foreach (Item item in itemsDelUsuario)
+                items.Add(item);
+
+            foreach (Item objeto in items)
             {
                 objeto.iluminar();
+            }
+        }
+
+        void construccion(TgcD3dInput input, float elapsedTime)
+        {
+            movimientoObjeto = new Vector3(0, 0, 0);
+            mouseVector = new Vector2(GuiController.Instance.D3dInput.Xpos, GuiController.Instance.D3dInput.Ypos);
+
+            evaluoClickEnMenu();
+
+            borrarSiClickea();
+
+            pickDragAndDrop(input, elapsedTime);
+             
+            anteriorMouse = mouseVector;
+            objetoAnterior = objetoPickeado;
+        }
+
+        private void evaluoClickEnMenu()
+        {
+            if (GuiController.Instance.D3dInput.buttonPressed(TgcViewer.Utils.Input.TgcD3dInput.MouseButtons.BUTTON_LEFT))
+            {
+                pickingRay.updateRay();
+                menu.poneloEnPantallaSiToco(mouseVector.X, mouseVector.Y);
+            } 
+        }
+
+        private void borrarSiClickea()
+        {
+            if (GuiController.Instance.D3dInput.buttonPressed(TgcViewer.Utils.Input.TgcD3dInput.MouseButtons.BUTTON_RIGHT))
+            {
+                pickingRay.updateRay();
+                foreach (Item objeto in itemsDelUsuario)
+                {
+                    TgcBoundingBox aabb = objeto.mesh.BoundingBox;
+                    selected = TgcCollisionUtils.intersectRayAABB(pickingRay.Ray, aabb, out collisionPoint);
+
+                    if (selected)
+                    {
+                        objeto.enEscena = false;
+                        objeto.pickeado = false;
+                        objeto.llevarAContenedor();
+                    }
+                }
+            }
+        }
+
+        private void pickDragAndDrop(TgcD3dInput input, float elapsedTime)
+        {
+            if (input.buttonDown(TgcD3dInput.MouseButtons.BUTTON_LEFT))
+            {
+                pickingRay.updateRay();
+
+                foreach (Item objeto in itemsDelUsuario)
+                {
+                    TgcBoundingBox aabb = objeto.mesh.BoundingBox;
+                    selected = TgcCollisionUtils.intersectRayAABB(pickingRay.Ray, aabb, out collisionPoint);
+
+                    if (selected && objeto.enEscena)
+                    {
+                        objetoPickeado = objeto;
+
+                        if (objetoAnterior != null)
+                            objetoAnterior.pickeado = false;
+                        objetoPickeado.pickeado = true;
+                        agarrado = true;
+
+                    }
+                }
+
+                if (agarrado)
+                {
+                    movimientoObjeto.X = -(mouseVector.X - anteriorMouse.X);
+                    movimientoObjeto.Y = -(mouseVector.Y - anteriorMouse.Y);
+
+                }
             }
 
-            foreach (Item objeto in itemsDeNivel)
+            if (input.buttonUp(TgcD3dInput.MouseButtons.BUTTON_LEFT))
             {
-                objeto.iluminar();
+                agarrado = false;
             }
+
+            if (agarrado && objetoPickeado.enEscena)
+            {
+                objetoPickeado.move(movimientoObjeto * 0.032f);
+            }
+
+            if (objetoPickeado != null && objetoPickeado.pickeado)
+            {
+                objetoPickeado.getOBB().render();
+                if (input.keyDown(Key.D))
+                {
+                    objetoPickeado.rotate(new Vector3(0, 0, 3 * elapsedTime));
+                }
+                if (input.keyDown(Key.A))
+                {
+                    objetoPickeado.rotate(new Vector3(0, 0, -3 * elapsedTime));
+                }
+            }
+            
+        }
+
+        void play(TgcD3dInput input, float elapsedTime)
+        {
+            pelota.interactuar(input, elapsedTime);
+
+            pelota.aplicarMovimientos(elapsedTime, items);
         }
 
         /// <summary>
@@ -158,16 +273,16 @@ namespace AlumnoEjemplos.MiGrupo
             else if (input.keyDown(Key.Space))
             {
                 etapa = play;
-                textStage.Text = etapa.getNombre();
+                textStage.Text = "Play";
             }
             else if (input.keyDown(Key.C))
             {
                 fabricaDeNiveles.reiniciar(numeroDeNivel, pelota);
                 etapa = construccion;
-                textStage.Text = etapa.getNombre();
+                textStage.Text = "Construccion";
             }
 
-            etapa.interaccion(input, elapsedTime);
+            etapa(input, elapsedTime);
 
             fondo.render();
             contenedor.render();
