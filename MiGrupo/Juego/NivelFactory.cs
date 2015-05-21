78fe44d6bdd49c6f86decd7f55f740d26941433c
lean93 <leanrodri2093@gmail.com>
2015-05-20 21:56:40 -0300

    modificando nivel
    level 2

--- MiGrupo/Juego/NivelFactory.cs
+++ MiGrupo/Juego/NivelFactory.cs
@@ -43,7 +43,11 @@ namespace AlumnoEjemplos.MiGrupo
         Cannon cannon2;
         Magnet magnet1;
         Magnet magnet2;
+        Magnet magnet3;
+        Magnet magnet4;
         Spring spring1;
+        Spring spring2;
+        Spring spring3;
 
 
         public static string alumnoTextureFolder()
@@ -62,11 +66,14 @@ namespace AlumnoEjemplos.MiGrupo
             lateralDerecha.dispose();
             lateralIzquierda.dispose();
             cannon.dispose();
-            cannon2.dispose();
+           // cannon2.dispose();
             magnet1.dispose();
             magnet2.dispose();
+            magnet3.dispose();
+            magnet4.dispose();
             spring1.dispose();
-
+            spring2.dispose();
+            spring3.dispose();
             scene.disposeAll();
         }
 
@@ -119,27 +126,38 @@ namespace AlumnoEjemplos.MiGrupo
                     break;
                 case 2:
 
-                    objetoGanador = TgcBox.fromSize(new Vector3(-10, -4, 1), new Vector3(1, 1, 1), Color.Blue);
-                    pelota = new Pelota(1f, new Vector3(10, 5, 1), madera);
+                    objetoGanador = TgcBox.fromSize(new Vector3(10, -4, 1), new Vector3(1, 1, 1), Color.Blue);
+                    pelota = new Pelota(0.5f, new Vector3(12, 8, 1), madera);
                     cartel.Texture = texturaGanaste;
                     //Items del Usuario 
                     itemsDelUsuario.Clear();
-                    cannon2 = Cannon.CrearCannon(scene.Meshes[0].clone("Cannon2"), scene.Meshes[2].clone("baseCannon1"), texturaCannon, Color.Black, new Vector3(0.1f, 0.1f, 0.1f), new Vector3(0, 0, pi / 4));
-                    itemsDelUsuario.Add(cannon2);
+                   // cannon2 = Cannon.CrearCannon(scene.Meshes[0].clone("Cannon2"), scene.Meshes[2].clone("baseCannon1"), texturaCannon, Color.Black, new Vector3(0.1f, 0.1f, 0.1f), new Vector3(0, 0, pi / 4));
+                    magnet3 = Magnet.CrearMagnet(scene.Meshes[3].clone("magnet1"), texturaMagnet, Color.Black, new Vector3(0.1f, 0.1f, 0.1f), new Vector3(0, 0, 0));
+                    magnet4 = Magnet.CrearMagnet(scene.Meshes[3].clone("magnet2"), texturaMagnet, Color.Black, new Vector3(0.1f, 0.1f, 0.1f), new Vector3(0, 0, pi));
+                    spring2 = Spring.CrearSpring(scene.Meshes[1].clone("spring1"), texturaSpring, Color.Black, new Vector3(0.1f, 0.1f, 0.1f), new Vector3(0, 0, 0));
+                    spring3 = Spring.CrearSpring(scene.Meshes[1].clone("spring1"), texturaSpring, Color.Black, new Vector3(0.1f, 0.1f, 0.1f), new Vector3(0, 0, 0));
+                    //itemsDelUsuario.Add(cannon2);
+                    itemsDelUsuario.Add(magnet3);
+                    itemsDelUsuario.Add(magnet4);
+                    itemsDelUsuario.Add(spring2);
+                    itemsDelUsuario.Add(spring3);
                     //Fin Items del Usuario  
                 
                     //Items del Nivel
                     itemsDeNivel.Clear();
                     setParedes(metal, itemsDeNivel);
                     Pared obstaculo1 = Pared.CrearPared(new Vector3(-10, -6, 1), new Vector3(5, 0.25f, 1), new Vector3(0, 0, 0), madera, "Obstaculo1");
-                    Pared obstaculo2 = Pared.CrearPared(new Vector3(-5, -4, 1), new Vector3(5, 0.25f, 1), new Vector3(0, 0, 0), madera, "Obstaculo2");
-                    Pared obstaculo3 = Pared.CrearPared(new Vector3(5, -2, 1), new Vector3(5, 0.25f, 1), new Vector3(0, 0, 0), madera, "Obstaculo3");
-                    Pared obstaculo4 = Pared.CrearPared(new Vector3(10, 0, 1), new Vector3(5, 0.25f, 1), new Vector3(0, 0, FastMath.PI / 4), madera, "Obstaculo4");
+                    Pared obstaculo2 = Pared.CrearPared(new Vector3(-4, 2, 1), new Vector3(6, 0.25f, 1), new Vector3(0, 0, 0), madera, "Obstaculo2");
+                    Pared obstaculo3 = Pared.CrearPared(new Vector3(7, 2, 1), new Vector3(6, 0.25f, 1), new Vector3(0, 0, 0), madera, "Obstaculo3");
+                    Pared obstaculo4 = Pared.CrearPared(new Vector3(12, 4, 1), new Vector3(5, 0.25f, 1), new Vector3(0, 0, FastMath.PI / 4), madera, "Obstaculo4");
+                    Pared obstaculo5 = Pared.CrearPared(new Vector3(-7, -1, 1), new Vector3(6, 0.25f, 1), new Vector3(0, 0, FastMath.PI / 2), madera, "Obstaculo5");
+
                     itemsDeNivel.Add(obstaculo1);
                     itemsDeNivel.Add(obstaculo2);
                     itemsDeNivel.Add(obstaculo3);
                     itemsDeNivel.Add(obstaculo4);
-                    itemsDeNivel.Add(cannon2.baseCannon);
+                    itemsDeNivel.Add(obstaculo5);
+                    //itemsDeNivel.Add(cannon2.baseCannon);
                     //Fin Items del Nivel
                     
                     fondo.setTexture(metal);
