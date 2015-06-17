4bbcc37a99d0e60316aee166bdc20ab93e6d6a2f
Meketh <NahJerSan@gmail.com>
2015-06-17 13:20:31 -0300

    AnimatedQuad

---
+++ SRC/AtTheEndOfTheDay/Particle/AnimatedQuad.cs
@@ -0,0 +1,140 @@
+﻿using Microsoft.DirectX;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using TgcViewer;
+using TgcViewer.Utils.TgcGeometry;
+using TgcViewer.Utils.TgcSceneLoader;
+using Microsoft.DirectX.Direct3D;
+
+namespace AlumnoEjemplos.AtTheEndOfTheDay.ThePerfectElement
+{
+    public class AnimatedQuad : IDisposable
+    {
+        #region Constructors
+        public Boolean _IsEnabled;
+        private Single _CurrentTime;
+        public AnimatedQuad()
+        {
+            IsPlaying = true;
+        }
+        #endregion Constructors
+
+        #region Properties
+        public Int32 FirstFrame { get; set; }
+        public Int32 CurrentFrame { get; set; }
+        public Boolean IsPlaying { get; set; }
+        private Single _AnimationLength;
+        private void UpdateAnimationLength()
+        {
+            _AnimationLength = _FrameRate == 0 ? 0 : (Single)_TotalFrames / _FrameRate;
+        }
+        private Int32 _TotalFrames;
+        public Int32 TotalFrames
+        {
+            get { return _TotalFrames; }
+            set
+            {
+                if (_TotalFrames == value) return;
+                _TotalFrames = value;
+                UpdateAnimationLength();
+            }
+        }
+        private Single _FrameRate;
+        public Single FrameRate
+        {
+            get { return _FrameRate; }
+            set
+            {
+                if (_FrameRate == value) return;
+                _FrameRate = value;
+                UpdateAnimationLength();
+            }
+        }
+        private TexturedQuad _TexturedQuad = new TexturedQuad() { IsAlphaBlendEnabled = true };
+        private Size _TextureSize = new Size(1, 1);
+        public TgcTexture Texture
+        {
+            get { return _TexturedQuad.Texture; }
+            set
+            {
+                _TexturedQuad.Texture = value;
+                _TextureSize = value == null ? new Size(1, 1) : value.Size;
+            }
+        }
+        public Vector3 Position
+        {
+            get { return _TexturedQuad.Position; }
+            set { _TexturedQuad.Position = value; }
+        }
+        public Vector2 Size
+        {
+            get { return _TexturedQuad.Size; }
+            set { _TexturedQuad.Size = value; }
+        }
+        public Matrix RotationMatrix
+        {
+            get { return _TexturedQuad.RotationMatrix; }
+            set { _TexturedQuad.RotationMatrix = value; }
+        }
+        private Size _TileInverse = new Size(1, 1);
+        private Size _FrameSize = new Size(1, 1);
+        private Int32 _RealTotalFrames = 1;
+        public Size FrameSize
+        {
+            get { return _FrameSize; }
+            set
+            {
+                if (_FrameSize == value) return;
+                _FrameSize = value;
+                var tile = _TexturedQuad.Tile = new Vector2((Single)value.Width / (Single)_TextureSize.Width, (Single)value.Height / (Single)_TextureSize.Height);
+                _TileInverse = new Size((Int32)(1 / tile.X), (Int32)(1 / tile.Y));
+                _RealTotalFrames = _TileInverse.Width * _TileInverse.Height;
+            }
+        }
+        #endregion Properties
+
+        #region AnimationMethods
+        public void Update(Single deltaTime)
+        {
+            if (!_IsEnabled) return;
+            if (IsPlaying)
+            {
+                _CurrentTime += deltaTime;
+                if (_TotalFrames != 0 && _CurrentTime > _AnimationLength)
+                {
+                    _CurrentTime = 0;
+                    _IsEnabled = false;
+                }
+            }
+            //Obtener cuadro actual
+            if (CurrentFrame != ((Int32)(_CurrentTime * FrameRate) + FirstFrame) % _RealTotalFrames)
+            {
+                CurrentFrame = (Int32)(_CurrentTime * FrameRate) + FirstFrame % _RealTotalFrames;
+                _TexturedQuad.UVOffset = new Vector2(_TexturedQuad.Tile.X * (CurrentFrame % _TileInverse.Width), _TexturedQuad.Tile.Y * (CurrentFrame / _TileInverse.Height));
+            }
+        }
+        public void Start()
+        {
+            _CurrentTime = 0;
+            _IsEnabled = true;
+        }
+        public void Stop()
+        {
+            _CurrentTime = 0;
+            _IsEnabled = false;
+        }
+        public void Render()
+        {
+            if (!_IsEnabled) return;
+            _TexturedQuad.Render();
+        }
+        public void Dispose()
+        {
+            _TexturedQuad.Dispose();
+        }
+        #endregion AnimationMethods
+    }
+}
---
+++ SRC/AtTheEndOfTheDay/Particle/TexturedQuad.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.DirectX;
+using Microsoft.DirectX.Direct3D;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using TgcViewer;
+using TgcViewer.Utils;
+using TgcViewer.Utils.Shaders;
+using TgcViewer.Utils.TgcSceneLoader;
+
+namespace AlumnoEjemplos.AtTheEndOfTheDay.ThePerfectElement
+{
+    public class TexturedQuad : IDisposable
+    {
+        #region Constructors
+        private Boolean _OldValues = true;
+        private CustomVertex.PositionTextured[] _Vertices = new CustomVertex.PositionTextured[6];
+        public TexturedQuad()
+        {
+            Enabled = true;
+            IsAlphaBlendEnabled = true;
+            Tile = new Vector2(1f, 1f);
+            Shader = GuiController.Instance.Shaders.VariosShader;
+            Technique = TgcShaders.T_POSITION_TEXTURED;
+        }
+        #endregion Constructors
+
+        #region Properties
+        public Effect Shader { get; set; }
+        public String Technique { get; set; }
+        public Vector2 Tile { get; set; }
+        public Vector2 UVOffset { get; set; }
+        public Boolean Enabled { get; set; }
+        public Boolean IsAlphaBlendEnabled { get; set; }
+        private void _SetAlphaBlend(Boolean isAlphaEnabled)
+        {
+            Device device = GuiController.Instance.D3dDevice;
+            device.RenderState.AlphaTestEnable = isAlphaEnabled;
+            device.RenderState.AlphaBlendEnable = isAlphaEnabled;
+        }
+        private Vector3 _Position;
+        public Vector3 Position
+        {
+            get { return _Position; }
+            set { _Position = value; _OldValues = true; }
+        }
+        private Vector2 _Size;
+        public Vector2 Size
+        {
+            get { return _Size; }
+            set { _Size = value; _OldValues = true; }
+        }
+        private Matrix _RotationMatrix = Matrix.Identity;
+        public Matrix RotationMatrix
+        {
+            get { return _RotationMatrix; }
+            set { _RotationMatrix = value; _OldValues = true; }
+        }
+        private TgcTexture _Texture;
+        public TgcTexture Texture
+        {
+            get { return _Texture; }
+            set
+            {
+                if (_Texture != null)
+                    _Texture.dispose();
+                _Texture = value;
+            }
+        }
+        public void Dispose()
+        {
+            Texture = null;
+        }
+        #endregion Properties
+
+        #region TextureMethods
+        public void Render()
+        {
+            if (!Enabled) return;
+            if (_OldValues)
+            {
+                _UpdateValues();
+                _OldValues = false;
+            }
+            Device d3dDevice = GuiController.Instance.D3dDevice;
+            TgcTexture.Manager texturesManager = GuiController.Instance.TexturesManager;
+            _SetAlphaBlend(IsAlphaBlendEnabled);
+            texturesManager.shaderSet(Shader, "texDiffuseMap", _Texture);
+            texturesManager.clear(1);
+            GuiController.Instance.Shaders.setShaderMatrixIdentity(this.Shader);
+            d3dDevice.VertexDeclaration = GuiController.Instance.Shaders.VdecPositionTextured;
+            Shader.Technique = Technique;
+            //Render con shader
+            Shader.Begin(0);
+            Shader.BeginPass(0);
+            d3dDevice.DrawUserPrimitives(PrimitiveType.TriangleList, 2, _Vertices);
+            Shader.EndPass();
+            Shader.End();
+            _SetAlphaBlend(false);
+        }
+        public void _UpdateValues()
+        {
+            //TODO:Calcular los 4 corners de la pared, segun el tipo de orientacion
+            Vector3 bLeft = new Vector3(-_Size.X / 2, -_Size.Y / 2, 0);
+            Vector3 bRight = new Vector3(_Size.X / 2, -_Size.Y / 2, 0);
+            Vector3 tLeft = new Vector3(-_Size.X / 2, _Size.Y / 2, 0);
+            Vector3 tRight = new Vector3(_Size.X / 2, _Size.Y / 2, 0);
+            //Primer triangulo
+            _Vertices[0] = new CustomVertex.PositionTextured(bLeft, UVOffset.X, UVOffset.Y + Tile.Y);
+            _Vertices[1] = new CustomVertex.PositionTextured(tLeft, UVOffset.X, UVOffset.Y);
+            _Vertices[2] = new CustomVertex.PositionTextured(tRight, UVOffset.X + Tile.X, UVOffset.Y);
+            //Segundo triangulo
+            _Vertices[3] = new CustomVertex.PositionTextured(bLeft, UVOffset.X, UVOffset.Y + Tile.Y);
+            _Vertices[4] = new CustomVertex.PositionTextured(tRight, UVOffset.X + Tile.X, UVOffset.Y);
+            _Vertices[5] = new CustomVertex.PositionTextured(bRight, UVOffset.X + Tile.X, UVOffset.Y + Tile.Y);
+            //Transformar todos los puntos
+            for (int i = 0; i < _Vertices.Length; i++)
+            {
+                _Vertices[i].Position = Vector3.TransformCoordinate(_Vertices[i].Position, _RotationMatrix);
+            }
+        }
+        #endregion TextureMethods
+    }
+}
